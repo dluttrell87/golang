715ab0230c682ae908c9193ef838560f3be0da80
alpha <dluttrell87@gmail.com>
2022-04-18 14:53:14 -0400

    test

---
+++ practice/main.go
@@ -0,0 +1,14 @@
+//test
+package main
+
+import "fmt"
+
+func main() {
+	var aString string = "Go man go"
+
+	fmt.Println(aString)
+	fmt.Printf("The variable type is %T", aString)
+
+	var anInt int = 42
+	fmt.Println("\n", anInt)
+}
